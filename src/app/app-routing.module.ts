a61721ae49e8ac2c295194b8b506b75a5f5fdc15
mbhossain <king.cseiu@gmail.com>
2022-11-04 20:28:29 +0600

    fixing

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,6 +2,7 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { AboutComponent } from './components/about/about.component';
 import { LandingPageComponent } from './components/landing-page/landing-page.component';
+import { LinksComponent } from './components/links/links.component';
 import { NowComponent } from './components/now/now.component';
 
 const routes: Routes = [
@@ -9,6 +10,7 @@ const routes: Routes = [
   { path: 'home', component: LandingPageComponent },
   { path: 'about', component: AboutComponent },
   { path: 'now', component: NowComponent },
+  { path: 'links', component: LinksComponent },
 ];
 
 @NgModule({
